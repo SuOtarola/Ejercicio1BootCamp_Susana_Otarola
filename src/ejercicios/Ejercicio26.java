96bdaedc84ac82c6ba9bfb4072953ceb6b8832a7
SuOtarola <susana.otarola@tsoftglobal.com>
2024-09-10 10:20:04 -0300

    Se agrega ejercicio 26

---
+++ src/ejercicios/Ejercicio26.java
@@ -0,0 +1,20 @@
+package ejercicios;
+
+public class Ejercicio26 {
+    public static void main(String[] args) {
+        /*26. Valor absoluto
+        •	Descripción: Determina el valor absoluto del número a = -15. El valor absoluto es la distancia de un número al 0, ignorando su signo.
+        •	Variables: a = -15.
+        */
+        int a = -15; // Número para el cual se desea calcular el valor absoluto
+
+        // Calcular el valor absoluto usando Math.abs()
+        int valorAbsoluto = Math.abs(a);
+
+        // Mostrar el resultado
+        System.out.println("El valor absoluto de " + a + " es " + valorAbsoluto);
+
+
+    }
+}
+
